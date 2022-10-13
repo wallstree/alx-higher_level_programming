284dec023f82c61dbc0bdf1443509e659e7fa0dc
wallstree <ianryol78@gmail.com>
2022-10-12 19:47:27 -0700

    workngpress

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-square.py Ian Ryol
+"""Defines a square """
+
+
+class Square:
+    """Does nothing"""
+    pass
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""My square module"""
+
+
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Create a Square
+         Args:
+         size: length of a side of Square
+         position: where the square is (coordinates)                                    """
+        self.size = size
+        self.position = position
+
+    def __str__(self):
+        self.my_print()
+
+    @property
+    def size(self):
+        """"The propery of size as the len of a side of Square
+        Raises:
+                TypeError: if size != int
+                ValueError: if size < 0 
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """property of the coordinates of this Square
+        Raises:
+            TypeError: if value != a tuple of 2 integers < 0
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set the position of this Square
+        Args: value as a tuple of two positive integers
+        Raises:
+        TypeError: if value is not a tuple or any int in tuple < 0
+        """
+        
+
+
+
+
